6e6a02d08e7a23abef134e00435a0533ce15469b
Victor Francisco <vfpresende@gmail.com>
2025-04-30 18:36:46 -0300

    minor fix. Correção do caminho para exportar csv

--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 RAW_EPW_DIR = BASE_DIR / 'raw' / 'epw_raw'
 RAW_INMET_DIR = BASE_DIR / 'raw' / 'inmet_raw'
-EXPORT_DIR = BASE_DIR / 'raw' / 'climate_csv'
+EXPORT_DIR = BASE_DIR / 'data_processed'
